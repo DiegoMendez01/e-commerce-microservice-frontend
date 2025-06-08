dee89f18c355b3977ac40b201143e833cbac3e59
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-06-08 16:42:41 -0500

    Modify colors and responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import logoLight from '/src/assets/comercio-electronico.svg';
 import logoDark from '/src/assets/comercio-electronico-negro.svg';
 import './Header.css';
 import LanguageSelector from '../LanguageSelector/LanguageSelector';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     const isDarkMode = useDarkMode();
@@ -16,22 +17,41 @@ export default function Header() {
 
             <header className="header">
                 <div className="header__logo">
-                    <img
-                        src={isDarkMode ? logoLight : logoDark}
-                        alt="Logo E-commerce"
-                        style={{ height: '40px' }}
-                    />
-                    <span className="header__app-name">E-commerce</span>
+                    <Link to="/" title="Ir a la página de inicio">
+                        <img
+                            src={isDarkMode ? logoLight : logoDark}
+                            alt="Logo E-commerce"
+                            title="Ir a Inicio"
+                            style={{ height: '40px' }}
+                        />
+                    </Link>
                 </div>
 
                 <nav className="header__socials" aria-label="Redes sociales">
-                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                    <a
+                        href="https://facebook.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Facebook"
+                        title="Ir a Facebook"
+                    >
                         <i className="fab fa-facebook-f" aria-hidden="true"></i>
                     </a>
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                    <a
+                        href="https://twitter.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Twitter"
+                        title="Ir a Twitter"
+                    >
                         <i className="fab fa-twitter" aria-hidden="true"></i>
                     </a>
-                    <a href="/sitemap" aria-label="Mapa del sitio" className="header__socials--sitemap">
+                    <a
+                        href="/sitemap"
+                        aria-label="Mapa del sitio"
+                        title="Ir a mapa del sitio"
+                        className="header__socials--sitemap"
+                    >
                         <i className="fas fa-sitemap" aria-hidden="true"></i>
                     </a>
                 </nav>
